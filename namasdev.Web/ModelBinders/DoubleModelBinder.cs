d5a537e4024ee276b8244c20742dfdafc8bd7484
Matias Lopez <matutelopez@hotmail.com>
2025-06-05 10:12:03 -0300

    [main] (ModelBinders) Agregar: double, integer, long, short

---
+++ namasdev.Web/ModelBinders/DoubleModelBinder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Web.Mvc;
+
+namespace namasdev.Web.ModelBinders
+{
+    public class DoubleModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+            if (value == null
+                || String.IsNullOrWhiteSpace(value.AttemptedValue))
+            {
+                return bindingContext.ModelType.IsGenericType
+                    ? (double?)null
+                    : default(double);
+            }
+
+            double valor;
+            if (!double.TryParse(value.AttemptedValue, NumberStyles.Any, CultureInfo.CurrentUICulture, out valor))
+            {
+                bindingContext.ModelState.AddModelError(bindingContext.ModelName, "Número no válido.");
+                return default(double);
+            }
+
+            return valor;
+        }
+    }
+}
---
+++ namasdev.Web/ModelBinders/IntegerModelBinder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Web.Mvc;
+
+namespace namasdev.Web.ModelBinders
+{
+    public class IntegerModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+            if (value == null
+                || String.IsNullOrWhiteSpace(value.AttemptedValue))
+            {
+                return bindingContext.ModelType.IsGenericType
+                    ? (int?)null
+                    : default(int);
+            }
+
+            int valor;
+            if (!int.TryParse(value.AttemptedValue, NumberStyles.Any, CultureInfo.CurrentUICulture, out valor))
+            {
+                bindingContext.ModelState.AddModelError(bindingContext.ModelName, "Número no válido.");
+                return default(int);
+            }
+
+            return valor;
+        }
+    }
+}
